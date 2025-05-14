64133802a23755f4c8c6d591dedc49bf64b3faaf
RuanPSilva <rpfernandes71@gmail.com>
2025-05-14 20:47:16 -0300

    Atualizando Controllers

---
+++ Projeto_Cid2/Controllers/ProdutoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto_Cid2.Models;
+using Projeto_Cid2.Repositorio;
+namespace Projeto_Cid2.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ProdutoRepositorio produtoRepositorio;
+
+        public ProdutoController(ProdutoRepositorio _produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Projeto_Cid2/obj/Debug/net8.0/Projeto_Cid2.AssemblyInfo.cs
+++ Projeto_Cid2/obj/Debug/net8.0/Projeto_Cid2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Cid2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c524a563419e2249bf53b6660a3619453b7522d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71ef0f9c04c9aca81e965a7aaa227e9bef7591f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Cid2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Cid2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
