da613237b80dfd72048432512f6612ea1f4afc08
RuanPSilva <rpfernandes71@gmail.com>
2025-05-14 20:01:30 -0300

    Criando o banco e adicionando o MYSQL

--- Projeto_Cid2/obj/Debug/net8.0/Projeto_Cid2.AssemblyInfo.cs
+++ Projeto_Cid2/obj/Debug/net8.0/Projeto_Cid2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Cid2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833398d0437ec8036cf0ef92b47e1c9fba7e59b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Cid2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Cid2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
